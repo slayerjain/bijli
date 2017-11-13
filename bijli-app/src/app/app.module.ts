a315878d32a1358ba6d491a46fcab1de2066cc0e
Shubham Jain <shubhamkjain@outlook.com>
2017-11-13 18:10:35 +0530

    Initial commit

---
+++ bijli-app/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { Configuration }     from './configuration';
+import { DataService }     from './data.service';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+// import { TransactionComponent } from './Transaction/Transaction.component'
+
+import { CoinsComponent } from './Coins/Coins.component';
+import { EnergyComponent } from './Energy/Energy.component';
+import { CashComponent } from './Cash/Cash.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+		HomeComponent,
+    // TransactionComponent,
+    CoinsComponent,
+		EnergyComponent,
+		
+    CashComponent
+		
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    AppRoutingModule
+  ],
+  providers: [
+    Configuration,
+    DataService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
